31b76cc2edeef5ecba1ed88101402bc7b380d0c0
Eldad Bercovici <dutzi.b@gmail.com>
2015-04-26 01:18:28 +0300

    Added photos to poster

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,28 +6,28 @@ angular.module('posterApp').controller('MainCtrl', function (
 	socket,
 	facebookService
 ) {
-	$scope.awesomeThings = [];
+	// $scope.awesomeThings = [];
 
-	$http.get('/api/things').success(function(awesomeThings) {
-		$scope.awesomeThings = awesomeThings;
-		socket.syncUpdates('thing', $scope.awesomeThings);
-	});
+	// $http.get('/api/things').success(function(awesomeThings) {
+	// 	$scope.awesomeThings = awesomeThings;
+	// 	socket.syncUpdates('thing', $scope.awesomeThings);
+	// });
 
-	$scope.addThing = function() {
-		if($scope.newThing === '') {
-			return;
-		}
-		$http.post('/api/things', { name: $scope.newThing });
-		$scope.newThing = '';
-	};
+	// $scope.addThing = function() {
+	// 	if($scope.newThing === '') {
+	// 		return;
+	// 	}
+	// 	$http.post('/api/things', { name: $scope.newThing });
+	// 	$scope.newThing = '';
+	// };
 
-	$scope.deleteThing = function(thing) {
-		$http.delete('/api/things/' + thing._id);
-	};
+	// $scope.deleteThing = function(thing) {
+	// 	$http.delete('/api/things/' + thing._id);
+	// };
 
-	$scope.$on('$destroy', function () {
-		socket.unsyncUpdates('thing');
-	});
+	// $scope.$on('$destroy', function () {
+	// 	socket.unsyncUpdates('thing');
+	// });
 
 	$scope.posts = [];
 
@@ -39,6 +39,15 @@ angular.module('posterApp').controller('MainCtrl', function (
 		});
 		$scope.posts = $scope.posts.concat(data);
 		$scope.isLoadingPosts = false;
+
+		// if (!$scope.selectedComment) {
+		// 	for (var i = 0; i < $scope.posts.length; i++) {
+		// 		if ($scope.posts[i].comments) {
+		// 			$scope.onCommentClick($scope.posts[i].comments.data[1]);
+		// 			break;
+		// 		}
+		// 	}
+		// }
 	}
 
 	$scope.loadMore = function () {
@@ -49,10 +58,88 @@ angular.module('posterApp').controller('MainCtrl', function (
 	facebookService.reset();
 	$scope.loadMore();
 
+	function updateIconSize() {
+		if (!$scope.iconType) { return; }
+
+		var width = $scope.posterWidth * $scope.iconType.ratio;
+		$scope.iconCSS = {
+			'width': width,
+			'margin-right': -width / 2,
+			'margin-top': -width / $scope.iconType.width * $scope.iconType.height / 2
+		};
+	}
+
 	$scope.onCommentClick = function (comment) {
 		console.log(comment);
 		$scope.selectedComment = comment;
+
+		// Choose a random icon type
+		//
+		$scope.iconType = icons[
+			Math.floor(Math.random() * icons.length)
+			// 3
+		];
+
+		// Choose a random layout
+		//
+		$scope.layout = layouts[
+			Math.floor(Math.random() * layouts.length)
+		].layout;
+
+		updateIconSize();
 	};
+
+	var icons = [
+		{
+			label: 'Pink hands',
+			ratio: 0.3,
+			width: 800,
+			height: 1448,
+			image: 'ok.png',
+			bgColor: '#8CC63E'
+		},
+		{
+			label: 'Blue hands',
+			ratio: 0.5,
+			width: 920,
+			height: 1156,
+			image: 'pray.png',
+			bgColor: '#F8ED31'
+		},
+		{
+			label: 'Heart',
+			ratio: 0.3,
+			width: 425,
+			height: 785,
+			image: 'heart.png',
+			bgColor: '#F8DB89'
+		},
+		{
+			label: 'Kaki',
+			ratio: 0.5,
+			width: 1137,
+			height: 1115,
+			image: 'kaki.png',
+			bgColor: '#C39A6B'
+		}
+	];
+
+	var layouts = [
+		{
+			layout: [
+				{ x: -0.01, y: -0.05 },
+				{ x: 0.70, y: 0.50 },
+				{ x: 0.30, y: 1.05 }
+			]
+		},
+		{
+			layout: [
+				{ x: 0.49, y: -0.04 },
+				{ x: 0.32, y: 0.50 },
+				{ x: 0.70, y: 1.03 }
+			]
+		}
+	];
 
 	// Not the prettiest thing, but will do for now.
 	//
@@ -67,6 +154,8 @@ angular.module('posterApp').controller('MainCtrl', function (
 
 		var height = width * A3_ASPECT;
 
+		$scope.posterWidth = width;
+
 		$('.poster-container').css({
 			width: width,
 			height: height,
@@ -77,17 +166,11 @@ angular.module('posterApp').controller('MainCtrl', function (
 			width: width,
 			height: height
 		});
+
+		updateIconSize();
+
+		$scope.$digest();
 	};
 
-	window.onresize();
-
-	// FB.api('/me/feed', function (response) {
-	//     $scope.posts = response;
-	//     debugger;
-	// });
-	// $scope.posts = [
-	//     {
-	//         message: 'hey'
-	//     }
-	// ];
-  });
+	setTimeout(window.onresize, 100);
+});
